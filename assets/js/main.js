c2a4834cc875d86ee309dc405349a591ae216413
mhitchi <mayhitchings@gmail.com>
2020-09-09 16:12:19 -0400

    sticky nav resizing

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ const elements = Array.from(document.getElementsByClassName('count')) // get the
 
 elements.forEach(el => {
     const final = parseInt(el.textContent, 10) // parse out the final number
-    const duration = 4000 // duration in ms
+    const duration = 2000 // duration in ms
     const step = ts => {
     if (!start) {
         start = ts
@@ -21,3 +21,19 @@ elements.forEach(el => {
     // start the animation
     requestAnimationFrame(step)
 })
+
+
+// When the user scrolls down 50px from the top of the document, resize the header's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("header").style.height = "150px";
+    document.getElementById("logo").style.height = "250px";
+    document.getElementById("logo").style.width = "250px";
+  } else {
+    document.getElementById("header").style.height = "250px";
+    document.getElementById("logo").style.height = "500px";
+    document.getElementById("logo").style.width = "500px";
+  }
+}
