d37b23160c338e140f26e902069e60cecf433db9
mhitchi <mayhitchings@gmail.com>
2020-09-10 11:59:53 -0400

    fixed logo dimensions

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,11 +29,11 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementById("header").style.height = "150px";
-    document.getElementById("logo").style.height = "175px";
+    document.getElementById("logo").style.height = "155px";
     document.getElementById("logo").style.width = "175px";
   } else {
     document.getElementById("header").style.height = "250px";
-    document.getElementById("logo").style.height = "500px";
+    document.getElementById("logo").style.height = "430px";
     document.getElementById("logo").style.width = "500px";
   }
 }
