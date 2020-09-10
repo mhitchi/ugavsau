f7ce01b1fc6773fd39e7069636342fd61a186ab9
mhitchi <mayhitchings@gmail.com>
2020-09-10 14:19:38 -0400

    added counter in header, counter not working

--- assets/js/data.js
+++ assets/js/data.js
@@ -15,10 +15,36 @@ $(document).ready(()=>{
             let aubDay = dateObj.getDate(aubDate);
             console.log(`${aubCount} donors as of September ${aubDay}, 2020.`);
 
-            $('#aubCount').append(`${aubCount}`)
-            $('#aubDate').append(`${aubDay}`)
+            $('.aubCount').append(`${aubCount}`)
+            $('.aubDate').append(`${aubDay}`)
         })
     }
 
     getAubData()
-})
+
+//     // COUNTER
+//     let start // set on the first step to the timestamp provided
+//     const elements = Array.from(document.getElementsByClassName('count')) // get the elements
+//     console.log(elements);
+
+//     elements.forEach(el => {
+//         const final = parseInt(el.textContent, 10) // parse out the final number
+//         const duration = 2000 // duration in ms
+//         const step = ts => {
+//         if (!start) {
+//             start = ts
+//         }
+//         // get the time passed as a fraction of total duration
+//         let progress = (ts - start) / duration 
+
+//         el.textContent = Math.floor(progress * final) // set the text
+//         if (progress < 1) {
+//             // if we're not 100% complete, request another animation frame
+//             requestAnimationFrame(step) 
+//         }
+//         }
+
+//         // start the animation
+//         requestAnimationFrame(step)
+//     })
+ })
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,19 @@
-let start // set on the first step to the timestamp provided
-const elements = Array.from(document.getElementsByClassName('count')) // get the elements
-
-elements.forEach(el => {
-    const final = parseInt(el.textContent, 10) // parse out the final number
-    const duration = 2000 // duration in ms
-    const step = ts => {
-    if (!start) {
-        start = ts
-    }
-    // get the time passed as a fraction of total duration
-    let progress = (ts - start) / duration 
-
-    el.textContent = Math.floor(progress * final) // set the text
-    if (progress < 1) {
-        // if we're not 100% complete, request another animation frame
-        requestAnimationFrame(step) 
-    }
-    }
-
-    // start the animation
-    requestAnimationFrame(step)
-})
-
 
 // When the user scrolls down 50px from the top of the document, resize the header's font size
 window.onscroll = function() {scrollFunction()};
+
+let navStats = Array.from(document.getElementsByClassName("nav-stats"));
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementById("header").style.height = "150px";
     document.getElementById("logo").style.height = "155px";
     document.getElementById("logo").style.width = "175px";
+    navStats.forEach(stat => stat.style.display = "none");
   } else {
     document.getElementById("header").style.height = "250px";
     document.getElementById("logo").style.height = "430px";
     document.getElementById("logo").style.width = "500px";
+    navStats.forEach(stat => stat.style.display = "block");
   }
 }
