a12eeddabd6f3e8013920b94fe1cc9a4f42deb5c
mhitchi <mayhitchings@gmail.com>
2020-09-10 10:57:09 -0400

    console logging reformatted date

--- assets/js/data.js
+++ assets/js/data.js
@@ -2,16 +2,20 @@ $(document).ready(()=>{
 
     let aubCount, ugaCount, aubCountStu, ugaCountStu, aubDate, ugaDate;
     let school;
+    let dateObj = new Date();
 
-    const getData = (school) => {
+    const getAubData = () => {
         $.ajax({
             url:'https://raw.githubusercontent.com/RAdrianKing/AUbeatweek/master/auburn.json',
             dataType: 'json',
             type: 'GET',
         }).then((response) => {
-            console.log(response);
+            aubCount = response.DonorTotal;
+            aubDate = response.LastUpdated;
+            let newAubDate = dateObj.getDate(aubDate);
+            console.log(`${aubCount} donors as of September ${newAubDate}, 2020.`);
         })
     }
 
-    getData("uga")
+    getAubData()
 })
