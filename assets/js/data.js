ec40a0a4ad85d2968d185ef81f006e2478d82747
mhitchi <mayhitchings@gmail.com>
2020-09-11 12:32:33 -0400

    fixed counter by moving code block into .then() in data.js

--- assets/js/data.js
+++ assets/js/data.js
@@ -28,6 +28,33 @@ $(document).ready(()=>{
                     //tried aubCount.toString(), .length returned as 3
             $('.aubCount').append(aubCount)
             $('.aubDate').append(aubDay)
+
+            // COUNTER
+            let start // set on the first step to the timestamp provided
+            const elements = Array.from(document.getElementsByClassName('count')) // get the elements
+            console.log(elements);
+
+            elements.forEach(el => {
+                const final = parseInt(el.textContent, 10) // parse out the final number
+                console.log(final);
+                const duration = 2000 // duration in ms
+                const step = ts => {
+                if (!start) {
+                    start = ts
+                }
+                // get the time passed as a fraction of total duration
+                let progress = (ts - start) / duration 
+
+                el.textContent = Math.floor(progress * final) // set the text
+                if (progress < 1) {
+                    // if we're not 100% complete, request another animation frame
+                    requestAnimationFrame(step) 
+                }
+                }
+
+                // start the animation
+                requestAnimationFrame(step)
+            })
         })
     }
 
