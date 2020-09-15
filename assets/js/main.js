c199f8af5bc840680b4c7bba774f165379fed2a6
mhitchi <mayhitchings@gmail.com>
2020-09-15 10:43:54 -0400

    first attempt at mobile font-sizes

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,7 @@
 window.onscroll = function() {scrollFunction()};
 
 let navStats = Array.from(document.getElementsByClassName("nav-stats"));
+let daysLeft = Array.from(document.getElementsByClassName("days-left"));
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
@@ -11,10 +12,12 @@ function scrollFunction() {
     document.getElementById("logo").style.height = "155px";
     document.getElementById("logo").style.width = "175px";
     navStats.forEach(stat => stat.style.display = "none");
+    daysLeft.forEach(item => item.style.margin = "250px 0 0 0");
   } else {
     document.getElementById("header").style.height = "250px";
     document.getElementById("logo").style.height = "430px";
     document.getElementById("logo").style.width = "500px";
     navStats.forEach(stat => stat.style.display = "block");
+    daysLeft.forEach(item => item.style.margin = "380px 0 0 0");
   }
 }
