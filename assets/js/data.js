2d93df9b10a34fef556e756d316cf075ac7ce65b
mhitchi <mayhitchings@gmail.com>
2020-09-15 16:06:34 -0400

    new favicon, pulling time from JSON

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,6 +1,6 @@
 $(document).ready(()=>{
 
-    let aubCount, ugaCount, aubCountStu, ugaCountStu, aubDate, ugaDate;
+    let aubCount, ugaCount, aubCountStu, ugaCountStu, aubDate, ugaDate, aubTime, ugaTime;
     let dateObj = new Date();
 
     const getAubData = () => {
@@ -13,21 +13,16 @@ $(document).ready(()=>{
             console.log(response);
             aubCount = response.DonorTotal;
             aubDate = response.LastUpdated;
-            let aubDay = dateObj.getDate(aubDate);
-            console.log(`${aubCount} donors as of September ${aubDay}, 2020.`);
             
-            //ERROR:
-                //typeof aubCount = number
-                //parseInt() in main.js throwing NaN
-                    //placing aubCount in template literals doesn't work
-                    //getting value from textContent, which is the string "100"
-                    //Number() instead of parseInt() returns 0;
-                    //adding + to el.textContent returns 0;
-                    //typeof(NaN) = number
-                    //encodeURIComponent(aubCount) returns 100
-                    //tried aubCount.toString(), .length returned as 3
+            let aubTimeArr = aubDate.split(" ");
+            aubTime = aubTimeArr[1];
+
+            let aubDay = dateObj.getDate(aubDate);
+            console.log(`${aubCount} donors as of ${aubTime} EDT on September ${aubDay}`);
+
             $('.aubCount').append(aubCount)
             $('.aubDate').append(aubDay)
+            $('.aubTime').append(aubTime)
 
             // COUNTER
             let start // set on the first step to the timestamp provided
