21cbee6e9699a36653fca7aca1a636cbb8eba681
mhitchi <mayhitchings@gmail.com>
2020-09-24 09:53:25 -0400

    getting correct numbers for uga, may be related to setTimeout

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,115 +1,116 @@
+let aubCount, ugaCount, aubCountStu, ugaCountStu, aubDate, ugaDate, aubTime, ugaTime;
+let dateObj = new Date();
+
+const getAubData = () => {
+
+    $.ajax({
+        url:'https://raw.githubusercontent.com/RAdrianKing/AUbeatweek/master/auburn.json',
+        dataType: 'json',
+        type: 'GET',
+    }).then((response) => {
+        console.log(response);
+        aubCount = response.DonorTotal;
+        aubCountStu = response.StudentTotal;
+        aubDate = response.LastUpdated;
+        
+        let aubTimeArr = aubDate.split(" ");
+        aubTime = aubTimeArr[1];
+
+        let aubDay = dateObj.getDate(aubDate);
+
+        $('.aubCount').append(aubCount);
+        $('.aubCountStu').append(aubCountStu);
+        $('.aubDate').append(aubDay);
+        $('.aubTime').append(aubTime);
+
+        // COUNTER
+        let start // set on the first step to the timestamp provided
+        const elements = Array.from(document.getElementsByClassName('aubCount')) // get the elements
+        console.log(elements);
+
+        elements.forEach(el => {
+            const final = parseInt(el.textContent, 10) // parse out the final number
+            console.log(final);
+            const duration = 2000 // duration in ms
+            const step = ts => {
+            if (!start) {
+                start = ts
+            }
+            // get the time passed as a fraction of total duration
+            let progress = (ts - start) / duration 
+
+            el.textContent = Math.floor(progress * final) // set the text
+            if (progress < 1) {
+                // if we're not 100% complete, request another animation frame
+                requestAnimationFrame(step) 
+            }
+            }
+
+            // start the animation
+            requestAnimationFrame(step)
+        })
+    })
+}
+
+const getUgaData = () => {
+
+    $.ajax({
+        url:'https://sandbox.dar.uga.edu/tanyac/gailconnector/givingweekdonorinfo.php',
+        dataType: 'json',
+        type: 'GET',
+    }).then((response) => {
+        console.log(response);
+        ugaCount = response.info.DonorGiftTotal;
+        ugaCountStu = response.info.StudentGiftTotal;
+        ugaDate = response.info.LastUpdated;
+        
+        let ugaTimeArr = ugaDate.split(" ");
+        ugaTime = ugaTimeArr[1];
+
+        let ugaDay = dateObj.getDate(ugaDate);
+        console.log(`${ugaCount} donors as of ${ugaTime} EDT on September ${ugaDay}`);
+
+        $('.ugaCount').append(ugaCount);
+        $('.ugaCountStu').append(ugaCountStu);
+        $('.ugaDate').append(ugaDay);
+        $('.ugaTime').append(ugaTime);
+
+        // COUNTER
+        let start // set on the first step to the timestamp provided
+        const elements = Array.from(document.getElementsByClassName('ugaCount')) // get the elements
+        console.log(elements);
+
+        elements.forEach(el => {
+            const final = parseInt(el.textContent, 10) // parse out the final number
+            console.log(final);
+            const duration = 2000 // duration in ms
+            const step = ts => {
+            if (!start) {
+                start = ts
+            }
+            // get the time passed as a fraction of total duration
+            let progress = (ts - start) / duration 
+
+            el.textContent = Math.floor(progress * final) // set the text
+            if (progress < 1) {
+                // if we're not 100% complete, request another animation frame
+                requestAnimationFrame(step) 
+            }
+            }
+
+            // start the animation
+            requestAnimationFrame(step)
+        })
+    })
+}
+
 $(document).ready(()=>{
 
-    let aubCount, ugaCount, aubCountStu, ugaCountStu, aubDate, ugaDate, aubTime, ugaTime;
-    let dateObj = new Date();
-
-    const getAubData = () => {
-    
-        $.ajax({
-            url:'https://raw.githubusercontent.com/RAdrianKing/AUbeatweek/master/auburn.json',
-            dataType: 'json',
-            type: 'GET',
-        }).then((response) => {
-            console.log(response);
-            aubCount = response.DonorTotal;
-            aubCountStu = response.StudentTotal;
-            aubDate = response.LastUpdated;
-            
-            let aubTimeArr = aubDate.split(" ");
-            aubTime = aubTimeArr[1];
-
-            let aubDay = dateObj.getDate(aubDate);
-
-            $('.aubCount').append(aubCount);
-            $('.aubCountStu').append(aubCountStu);
-            $('.aubDate').append(aubDay);
-            $('.aubTime').append(aubTime);
-
-            // COUNTER
-            let start // set on the first step to the timestamp provided
-            const elements = Array.from(document.getElementsByClassName('aubCount')) // get the elements
-            console.log(elements);
-
-            elements.forEach(el => {
-                const final = parseInt(el.textContent, 10) // parse out the final number
-                console.log(final);
-                const duration = 2000 // duration in ms
-                const step = ts => {
-                if (!start) {
-                    start = ts
-                }
-                // get the time passed as a fraction of total duration
-                let progress = (ts - start) / duration 
-
-                el.textContent = Math.floor(progress * final) // set the text
-                if (progress < 1) {
-                    // if we're not 100% complete, request another animation frame
-                    requestAnimationFrame(step) 
-                }
-                }
-
-                // start the animation
-                requestAnimationFrame(step)
-            })
-        })
-    }
-
-    const getUgaData = () => {
-    
-        $.ajax({
-            url:'https://sandbox.dar.uga.edu/tanyac/gailconnector/givingweekdonorinfo.php',
-            dataType: 'json',
-            type: 'GET',
-        }).then((response) => {
-            console.log(response);
-            ugaCount = response.info.DonorGiftTotal;
-            ugaCountStu = response.info.StudentGiftTotal;
-            ugaDate = response.info.LastUpdated;
-            
-            let ugaTimeArr = ugaDate.split(" ");
-            ugaTime = ugaTimeArr[1];
-
-            let ugaDay = dateObj.getDate(ugaDate);
-            console.log(`${ugaCount} donors as of ${ugaTime} EDT on September ${ugaDay}`);
-
-            $('.ugaCount').append(ugaCount);
-            $('.ugaCountStu').append(ugaCountStu);
-            $('.ugaDate').append(ugaDay);
-            $('.ugaTime').append(ugaTime);
-
-            // COUNTER
-            let start // set on the first step to the timestamp provided
-            const elements = Array.from(document.getElementsByClassName('ugaCount')) // get the elements
-            console.log(elements);
-
-            elements.forEach(el => {
-                const final = parseInt(el.textContent, 10) // parse out the final number
-                console.log(final);
-                const duration = 2000 // duration in ms
-                const step = ts => {
-                if (!start) {
-                    start = ts
-                }
-                // get the time passed as a fraction of total duration
-                let progress = (ts - start) / duration 
-
-                el.textContent = Math.floor(progress * final) // set the text
-                if (progress < 1) {
-                    // if we're not 100% complete, request another animation frame
-                    requestAnimationFrame(step) 
-                }
-                }
-
-                // start the animation
-                requestAnimationFrame(step)
-            })
-        })
-    }
     getUgaData();
     getAubData();
 
     //set timer to update ugaData every minute
-    setTimeout(()=> {
-        getUgaData();
-    }, 1000);
+//     setTimeout(()=> {
+//         getUgaData();
+//     }, 1000);
 })
