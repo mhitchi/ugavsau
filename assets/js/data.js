983c0f4a6f44cb35bc1480f351bf60a4527ced7a
mhitchi <mayhitchings@gmail.com>
2020-09-25 09:43:53 -0400

    still not counting, but accessing elements by class name

--- assets/js/data.js
+++ assets/js/data.js
@@ -49,33 +49,35 @@ const getData = (name, jsonUrl) => {
         $(`.${name}CountStu`).append(countStu);
         $(`.${name}Date`).append(day);
         $(`.${name}Time`).append(time);
+    })
+}
 
-        // COUNTER
-        let start // set on the first step to the timestamp provided
-        const elements = Array.from(document.getElementsByClassName(`.${name}Count`)) // get the elements
-        console.log(elements);
+const count = () => {
+    // COUNTER
+    let start // set on the first step to the timestamp provided
+    const elements = Array.from(document.getElementsByClassName('count')) // get the elements
+    console.log(elements);
 
-        elements.forEach(el => {
-            const final = parseInt(el.textContent, 10) // parse out the final number
-            console.log(final);
-            const duration = 2000 // duration in ms
-            const step = ts => {
-            if (!start) {
-                start = ts
-            }
-            // get the time passed as a fraction of total duration
-            let progress = (ts - start) / duration 
+    elements.forEach(el => {
+        const final = parseInt(el.innerHTML, 10) // parse out the final number
+        console.log(final);
+        const duration = 2000 // duration in ms
+        const step = ts => {
+        if (!start) {
+            start = ts
+        }
+        // get the time passed as a fraction of total duration
+        let progress = (ts - start) / duration 
 
-            el.textContent = Math.floor(progress * final) // set the text
-            if (progress < 1) {
-                // if we're not 100% complete, request another animation frame
-                requestAnimationFrame(step) 
-            }
-            }
+        el.textContent = Math.floor(progress * final) // set the text
+        if (progress < 1) {
+            // if we're not 100% complete, request another animation frame
+            requestAnimationFrame(step) 
+        }
+        }
 
-            // start the animation
-            requestAnimationFrame(step)
-        })
+        // start the animation
+        requestAnimationFrame(step)
     })
 }
 
@@ -87,4 +89,5 @@ $(document).ready(()=>{
 
         getData(name, jsonUrl);
     })
+    count()
 })
